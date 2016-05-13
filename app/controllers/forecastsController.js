a0af23e5e8c7b60f8960c81fcee1712d4661bef1
TarikFojnica <tarik.f786@gmail.com>
2016-05-13 16:01:06 +0200

    Clean up the code

--- app/controllers/forecastsController.js
+++ app/controllers/forecastsController.js
@@ -6,27 +6,8 @@ App.controller('forecastsController', function ($scope, Data, ConvertData) {
 	$scope.location.city = 'Sarajevo';
 	$scope.location.lat = '43.8563';
 	$scope.location.lng = '18.4131';
-
 	$scope.location.date = moment().format('Do MMMM YYYY');
-	var last30Dates = [];
 	$scope.switch = 'today';
-
-	// Get the dates of the last 30 days by taking the advantage of Moment.js library.
-	// After the date is received convert it into a Unix format and store in the array
-	// so we have it ready in the memory.
-
-	// NOTE: This functionality is now moved on the server and here is for the reference only , check:
-	// https://github.com/TarikFojnica/weather-app-server/blob/master/routes/forecasts.js
-
-	function getLast30Dates() {
-		for (var i = 0; i <= 29; i++) {
-			//taking unix time of each day in the last 30 days and storing it in the 'last30Dates' array
-			last30Dates.push(Math.floor((moment().subtract(i, 'days')) / 1000));
-		}
-	}
-
-	// Get the weather forecasts of the last 30 days. More logic on server:
-	// https://github.com/TarikFojnica/weather-app-server/blob/master/routes/forecasts.js
 
 	$scope.getLast30DaysForecasts = function (lat, lng) {
 		Data.last30DaysForecasts(lat, lng)
@@ -77,6 +58,5 @@ App.controller('forecastsController', function ($scope, Data, ConvertData) {
 
 	//call the initial default data on the app load
 	$scope.getTodayForecasts($scope.location.lat, $scope.location.lng);
-
 
 });
